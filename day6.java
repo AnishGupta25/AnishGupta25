7c5fdce452fe273fa30deae36693756b44d3812d
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-08 15:47:05 +0530

    Q2 done

--- day6.java
+++ day6.java
@@ -24,4 +24,34 @@ public class day6 {
         
         return head.data;
     }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode r = res;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                r.next = l1;
+                l1 = l1.next;
+                r = r.next;
+            }
+            else if(l2.val < l1.val){
+                r.next = l2;
+                l2 = l2.next;
+                r = r.next;
+            }
+        }
+        
+        while(l1 != null){
+            r.next = l1;
+            l1 = l1.next;
+            r = r.next;
+        }
+
+        while(l2 != null){
+            r.next = l2;
+            l2 = l2.next;
+            r = r.next;
+        }
+        return res.next;
+    }
 }
