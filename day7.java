d2053448692f49a0036071dcec676830de95ff8c
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-09 17:18:06 +0530

    Q8 done

--- day7.java
+++ day7.java
@@ -56,4 +56,17 @@ public class day7 {
         }
         return res;
     }
+
+    public static void pzz(int n) {
+        if(n == 1){
+            System.out.print("1 1 1 ");
+            return;
+        }
+        
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n- 1);
+        System.out.print(n + " ");
+    }
 }
