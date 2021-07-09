dc4398d8bcdb09f26749e66c9d15c2b5937f348a
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-09 16:56:10 +0530

    Q3 done

--- day7.java
+++ day7.java
@@ -16,4 +16,14 @@ public class day7 {
         printIncreasing(n - 1);
         System.out.println(n);
     }
+
+    public static void pdi(int n){
+        if(n == 0){
+            return;
+        }
+        
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
 }
