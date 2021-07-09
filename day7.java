2f8a242d7e4ecce07c6ab140b7d4657856bab8e2
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-09 17:25:59 +0530

    Q8 done

--- day7.java
+++ day7.java
@@ -69,4 +69,14 @@ public class day7 {
         pzz(n- 1);
         System.out.print(n + " ");
     }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+        if(n == 0){
+            return;
+        }
+        
+        toh(n-1, t1id , t3id , t2id);
+        System.out.println(n + "[" + t1id + " -> " + t2id + "]");
+        toh(n-1 , t3id , t2id , t1id);
+    }
 }
