7573a87bfa894ef9be6dcfebea1c414ad4187a61
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-10 17:25:39 +0530

    Q2 done

--- day8.java
+++ day8.java
@@ -13,5 +13,17 @@ public class day8 {
         }
     }
 
+    public static int firstIndex(int[] arr, int idx, int x) {
+        if (idx == arr.length) {
+          return -1;
+        }
     
+        if (arr[idx] == x) {
+          return idx;
+        }
+        else {
+          int fst = firstIndex(arr, idx - 1, x);
+          return fst;
+        }
+    }    
 }
