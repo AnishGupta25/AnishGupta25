358b47fcce55f603ce074b016596e5e067065947
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-11 17:26:04 +0530

    Q2 done

--- day9.java
+++ day9.java
@@ -20,4 +20,29 @@ public class day9 {
         
         return res;
     }
+
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if (sr == dr && sc == dc) {
+          ArrayList<String> my = new ArrayList<>();
+          my.add("");
+          return my;
+        }
+    
+        ArrayList<String> myPath = new ArrayList<>();
+        if (sc <= dc) {
+          ArrayList<String> hpath = getMazePaths(sr, sc + 1, dr, dc);
+          for (String path : hpath) {
+            myPath.add("h" + path);
+          }
+        }
+    
+        if (sr <= dr) {
+          ArrayList<String> vpath = getMazePaths(sr + 1, sc, dr, dc);
+          for (String path : vpath) {
+            myPath.add("v" + path);
+          }
+        }
+    
+        return myPath;
+    }
 }
