fba4845d719808c7a11b2f6c87a07a15d2fba9bd
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-20 14:35:35 +0530

    done

--- day17.java
+++ day17.java
@@ -25,4 +25,32 @@ public class day17 {
             this.tiltOfTree = tiltOfTree;
         }
     }
+
+    boolean isBST(Node root){
+        BSTpair tree = IsBst(root);
+        return tree.bst;
+    }
+    
+    public BSTpair IsBst(Node node){
+        if(node == null) return new BSTpair(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        
+        BSTpair lpair = IsBst(node.left);
+        BSTpair rpair = IsBst(node.right);
+        
+        int max = Math.max(node.data , Math.max(lpair.max , rpair.max));
+        int min = Math.min(node.data , Math.max(lpair.min , rpair.min));
+        boolean bst = lpair.bst && rpair.bst && node.data < rpair.min && node.data > lpair.max;
+        
+        return new BSTpair(max , min , bst);
+    }
+    
+    public class BSTpair{
+        int max , min;
+        Boolean bst;
+        BSTpair(int max, int min, boolean bst){
+            this.max = max;
+            this.min = min;
+            this.bst = bst;
+        }
+    }
 }
