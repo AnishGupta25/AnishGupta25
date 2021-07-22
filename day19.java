30d02d5f100daf217fa50b140f861142b943862c
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-22 17:34:20 +0530

    done

--- day19.java
+++ day19.java
@@ -18,4 +18,36 @@ public class day19 {
 	    
 	    return arr;
 	} 
+
+    ArrayList<Integer> inOrder(Node root){
+        Stack<TreePair> st = new Stack<>();
+        st.push(new TreePair(root , 0));
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        while(st.size() > 0){
+            TreePair top = st.peek();
+            if(top.state == 0){
+                top.state++;
+                if(top.root.left != null) st.push(new TreePair(top.root.left , 0));
+            }
+            else if(top.state == 1){
+                res.add(top.root.data);
+                top.state++;
+                if(top.root.right != null) st.push(new TreePair(top.root.right , 0));
+            }
+            else{
+                st.pop();
+            }
+        }
+        return res;
+    }
+    
+    class TreePair{
+        Node root;
+        int state;
+        TreePair(Node root, int state){
+            this.root = root;
+            this.state = state;
+        }
+    }
 }
