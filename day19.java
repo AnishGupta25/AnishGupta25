461f6fa20a0aed8ca5683f8403341a5e0d3ce3d7
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-22 17:12:06 +0530

    done

--- day19.java
+++ day19.java
@@ -1,3 +1,21 @@
 public class day19 {
-    
+    static int[] SortBinaryArray(int arr[], int n) { 
+	    int i = 0, j = 0;
+	    
+	    while(i < n){
+	        if(arr[i] == 1){
+	            i++;
+	        }
+	        else{
+	            int temp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = temp;
+	            
+	            j++;
+	            i++;
+	        }
+	    }
+	    
+	    return arr;
+	} 
 }
