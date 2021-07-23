2d99dea46cf0e7b0dd5b9ec62ebf68600767b468
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-23 20:00:13 +0530

    done

--- day20.java
+++ day20.java
@@ -29,4 +29,41 @@ public class day20 {
         }
         else return node;
     }
+
+    public static int size(Node node) {
+        if(node == null) return 0;
+        int left = size(node.left);
+        int right = size(node.right);
+        return left + right + 1;
+    }
+    
+    public static int sum(Node node) {
+        if(node == null) return 0;
+        int left = sum(node.left);
+        int right = sum(node.right);
+        return left + right + node.data;
+    }
+    
+    public static int max(Node node) {
+        if(node.right == null) return node.data;
+        int right = max(node.right);
+        return right;
+    }
+    
+    public static int min(Node node) {
+        if(node.left == null) return node.data;
+        int left = min(node.left);
+        return left;
+    }
+    
+    public static boolean find(Node node, int data){
+        if(node == null) return false;
+        if(data < node.data){
+            return find(node.left , data);
+        }
+        else if(data > node.data){
+            return find(node.right , data);
+        }
+        else return true;
+    }  
 }
