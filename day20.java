90aa2b5f9e15fa7f3405e3614962c7b283c9c996
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-23 19:58:11 +0530

    done

--- day20.java
+++ day20.java
@@ -15,4 +15,18 @@ public class day20 {
         }
         return false;
     }
+
+    public static Node add(Node node, int data) {
+        if(node == null) return new Node(data , null , null);
+        
+        if(data < node.data){
+            node.left = add(node.left , data);
+            return node;
+        }
+        else if(data > node.data){
+            node.right = add(node.right , data);
+            return node;
+        }
+        else return node;
+    }
 }
