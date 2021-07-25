82b998a4507529bd4a32dc35a063c108defdc72e
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-26 02:48:46 +0530

    done

--- day23.java
+++ day23.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class day23 {
     static int canRepresentBST(int arr[], int N) {
         Stack<Integer> st = new Stack<>();
@@ -13,5 +17,25 @@ public class day23 {
             st.push(arr[i]);
         }
         return 1;
+    }
+
+    ArrayList<Integer> leftView(Node root){
+        if(root == null) return new ArrayList<>();
+    
+        ArrayList<Integer> res = new ArrayList<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.addLast(root);
+        
+        while(queue.size() > 0){
+            int size = queue.size();
+            res.add(queue.getFirst().data);
+            while(size --> 0){
+                Node rn = queue.removeFirst();
+                if(rn.left != null) queue.addLast(rn.left );
+                if(rn.right != null) queue.addLast(rn.right);
+            }
+            
+        }
+        return res;
     }
 }
