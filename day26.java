86c2b3b21098435f4a26597b465dc94eebda49d5
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-29 19:38:57 +0530

    done

--- day26.java
+++ day26.java
@@ -22,4 +22,19 @@ public class day26 {
         }
         return res;
     }
+
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty()) return 0;
+        if(haystack.length() < needle.length()) return -1;
+        int n = haystack.length() , m = needle.length();
+        
+        for(int i = 0; i <= n - m; i++){
+            int j;
+            for(j = 0; j < m; j++){
+                if(haystack.charAt(i + j) != needle.charAt(j)) break;
+            }
+            if(j == m) return i;
+        }
+        return -1;
+    }
 }
