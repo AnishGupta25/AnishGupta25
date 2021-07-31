997d8327d33f86ab57712d66a48d90e48c01a7f6
AnishGupta25 <anishgupta2522@gmail.com>
2021-07-31 21:03:19 +0530

    done

--- day28.java
+++ day28.java
@@ -106,4 +106,28 @@ public class day28 {
         floodFillHelper(image , sr + 1 , sc , newColor , visited , prev);
         floodFillHelper(image , sr , sc - 1 , newColor , visited , prev);
     }
+
+    public int trap(int[] height) {
+        if(height.length == 0) return 0;
+        int trapedWater = 0;
+        
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        
+        left[0] = height[0];
+        right[height.length - 1] = height[height.length - 1];
+        
+        for(int i = 1; i < height.length; i++){
+            left[i] = Math.max(left[i - 1] , height[i]);
+        }
+        
+        for(int i = height.length - 2; i >= 0 ; i--){
+            right[i] = Math.max(right[i + 1] , height[i]);
+        }
+        
+        for(int i = 0; i < height.length; i++){
+            trapedWater += Math.min(left[i] , right[i]) - height[i];
+        }
+        return trapedWater;
+    }
 }
