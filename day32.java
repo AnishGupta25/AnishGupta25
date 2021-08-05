bd699ff6a63d8fae28049e49fcb913a58d2454cb
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-05 12:32:21 +0530

    done

--- day32.java
+++ day32.java
@@ -16,4 +16,22 @@ public class day32 {
         }
         return false;
     }
+
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        
+        int[] prime = {2 , 3 , 5};
+        int i = 0;
+        while(i < 3){
+            while(n % prime[i] == 0){
+                n /= prime[i];
+            }
+            i++;
+        }
+        
+        if(n == 1) return true;
+        
+        return false;
+    }
 }
