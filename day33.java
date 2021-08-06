e9f51dc3113f705ed7c15ed1d5a6d3c2934f26f4
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-06 18:00:04 +0530

    done

--- day33.java
+++ day33.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class day33 {
     int isPlaindrome(String S) {
         int n = S.length();
@@ -9,5 +10,20 @@ public class day33 {
             j--;
         }
         return 1;
+    }
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        if(triangle.size() == 1) return triangle.get(0).get(0);
+        int dp[] = new int[triangle.size()];
+        
+        for(int i = 0; i < triangle.size(); i++){
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for(int i = triangle.size() - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j] , dp[j+1]);
+            } 
+        }
+        
+        return dp[0];
     }
 }
