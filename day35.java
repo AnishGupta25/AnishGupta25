4fcc54441565c801c77dbd94fa03703c0c19dca3
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-08 02:31:11 +0530

    done

--- day35.java
+++ day35.java
@@ -40,4 +40,23 @@ public class day35 {
         }
         return dummy.next;
     }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) return;
+        int i = m - 1, j = n - 1 , k = m + n - 1;
+        while(i >= 0 || j >= 0){
+            int val1 = i < 0 ? Integer.MIN_VALUE : nums1[i];
+            int val2 = j < 0 ? Integer.MIN_VALUE : nums2[j];
+            if(val1 >= val2){
+                nums1[k] = val1;
+                k--;
+                i--;
+            }
+            else{
+                nums1[k] = val2;
+                j--;
+                k--;
+            }
+        }
+    }
 }
