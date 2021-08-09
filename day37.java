4d6286df175bcef1d5932d2e9ec46cb6e4fa9a58
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-10 02:08:59 +0530

    done

--- day37.java
+++ day37.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class day37 {
     public int minimumEffort(int[][] tasks) {
         Arrays.sort(tasks, (a , b)->{
@@ -15,5 +17,25 @@ public class day37 {
             cur_energy -= tasks[i][0];
         }
         return initial;
+    }
+
+    public Node connect(Node root) {
+        if(root == null) return root;
+        helper(root);
+        return root;
+    }
+    
+    public void helper(Node node){
+        if(node == null) return;
+        
+        if(node.left != null && node.right != null){
+            node.left.next = node.right;
+        }
+        if(node.next != null && node.right != null){
+            node.right.next = node.next.left;
+        }
+        
+        helper(node.left);
+        helper(node.right);
     }
 }
