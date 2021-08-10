0c084a9a244d1d30ef070bdea878a4314def432b
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-11 02:11:12 +0530

    done

--- day38.java
+++ day38.java
@@ -18,4 +18,24 @@ public class day38 {
         }
         return node;
     }
+
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+        
+        while(node2 != null){
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
+            
+            if(node2.next != null){
+                node1 = node2.next;
+                node2 = node1.next;
+            }
+            else node2 = node2.next;
+        }
+        return head;
+    }
 }
