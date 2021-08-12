bc7e7e7ec41bc6035b1d48bb314008b3c02424f5
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-12 14:40:34 +0530

    done

--- day39.java
+++ day39.java
@@ -107,4 +107,18 @@ public class day39 {
         }
         return size;
     }
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
 }
