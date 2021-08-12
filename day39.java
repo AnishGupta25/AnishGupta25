f45cedd887991c1328b1702a448a9f90722ae898
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-12 22:07:20 +0530

    done

--- day39.java
+++ day39.java
@@ -161,4 +161,25 @@ public class day39 {
         }
         curr.next = prev;
     }
+
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                while(head != slow){
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 }
