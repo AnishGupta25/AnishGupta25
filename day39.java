3a2f8a5b1656d19768d018a492763242969ede03
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-12 12:50:21 +0530

    done

--- day39.java
+++ day39.java
@@ -46,4 +46,31 @@ public class day39 {
         }
         return prev;
     }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode res = new ListNode(-1);
+        ListNode itr = res;
+        
+        ListNode cur = head;
+        ListNode fwd = head.next;
+        
+        while(cur != null){
+            boolean repeat = false;
+            while(fwd != null && cur.val == fwd.val){
+                repeat = true;
+                cur = fwd;
+                fwd = cur.next;
+            }
+            if(!repeat){
+                itr.next = new ListNode(cur.val);
+                itr = itr.next;
+            }
+            cur = fwd;
+            fwd = (cur != null ? cur.next : null);
+        }
+        
+        return res.next;
+    }
 }
