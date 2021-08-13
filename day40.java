1c553ae957c909305c85f1781246ac30d38f1acd
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-13 13:54:56 +0530

    done

--- day40.java
+++ day40.java
@@ -42,4 +42,18 @@ public class day40 {
         odd.next = even;
         return head;
     }
+
+    public int numTrees(int n) {
+        int dp[] = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i - j - 1]; 
+            } 
+        }
+        return dp[n];
+    }
 }
