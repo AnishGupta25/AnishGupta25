852231fc54b821dc06e73301636be1d5f13f3559
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-13 21:53:50 +0530

    done

--- day40.java
+++ day40.java
@@ -56,4 +56,13 @@ public class day40 {
         }
         return dp[n];
     }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null){
+            if(root.val == targetSum) return true;
+            else return false;
+        }
+        return hasPathSum(root.left , targetSum - root.val) || hasPathSum(root.right , targetSum - root.val);
+    }
 }
