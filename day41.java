d8bf50c8ff489f54517c3e94ae035734a8ec80c9
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-14 17:06:47 +0530

    done

--- day41.java
+++ day41.java
@@ -78,4 +78,16 @@ public class day41 {
         }
         return size;
     }
+
+    public int findDuplicate(int[] nums) {
+        int[] arr = new int[nums.length + 1];
+        
+        for(int i = 0; i < nums.length; i++){
+            arr[nums[i]] = arr[nums[i]] + 1;
+            
+            if(arr[nums[i]] == 2) return nums[i];
+        }
+        
+        return 0;
+    }
 }
