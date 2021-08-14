93c21dfdd907386e432ef21e4f2c2d4af4fd7cee
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-14 23:01:00 +0530

    done

--- day41.java
+++ day41.java
@@ -156,4 +156,27 @@ public class day41 {
         }
         return 0;
     }
+
+    static int trappingWater(int arr[], int n) { 
+        if(n <= 2) return 0;
+        
+        int left[] = new int[n];
+        left[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            left[i] = Math.max(left[i-1] , arr[i]);
+        }
+        
+        int right[] = new int[n];
+        right[n - 1] = arr[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            right[i] = Math.max(right[i+1] , arr[i]);
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += Math.min(left[i] , right[i]) - arr[i];
+        }
+        
+        return sum;
+    } 
 }
