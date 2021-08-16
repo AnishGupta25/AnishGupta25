53af31163b093d7a207739899813b8dcef56f1c9
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-16 18:08:14 +0530

    done

--- day43.java
+++ day43.java
@@ -12,4 +12,24 @@ public class day43 {
         }
         return prev;
     }
+
+    public Node pairwiseSwap(Node head){
+        if(head == null || head.next == null) return head;
+        
+        Node slow = head;
+        Node fast = head.next;
+        head = fast;
+        while(true){
+            Node fwd = fast.next;
+            fast.next = slow;
+            if(fwd == null || fwd.next == null){
+                slow.next = fwd;
+                break;
+            }
+            slow.next = fwd.next;
+            slow = fwd;
+            fast = slow.next;
+        }
+        return head;
+    }
 }
