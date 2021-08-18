a4e0cd837223ff74d9f328a666f1a9efbaf778cb
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-18 23:53:54 +0530

    done

--- day45.java
+++ day45.java
@@ -67,4 +67,17 @@ public class day45 {
         
         visited[row][col] = false;
     }
+
+    int intersectPoint(ListNode head1, ListNode head2){
+        // if(head1 == null || head2 == null) return null;
+         
+        ListNode temp1 = head1;
+        ListNode temp2 = head2;
+         
+         while(temp1 != temp2){
+             temp1 = temp1 == null ? head2 : temp1.next;
+             temp2 = temp2 == null ? head1 : temp2.next;
+         }
+         return temp1.val;
+     }
 }
