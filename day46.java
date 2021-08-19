e3121ed934c795caa17d10faebb554b6db6ff115
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-19 23:41:21 +0530

    done

--- day46.java
+++ day46.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class day46 {
     public int countGoodRectangles(int[][] rectangles) {
         int arr[] = new int[rectangles.length];
@@ -51,5 +53,21 @@ public class day46 {
             if(max > MaxSum) MaxSum = max;
         }
         return MaxSum;
+    }
+
+    public static boolean find3Numbers(int A[], int n, int X) { 
+        Arrays.sort(A);
+        
+        for(int i = 0; i < n; i++){
+            int l = i+1;
+            int r = n - 1;
+            
+            while(l < r){
+                if(A[i] + A[l] + A[r] == X) return true;
+                else if(A[i] + A[l] + A[r] > X) r--;
+                else l++;
+            }
+        }
+        return false;
     }
 }
