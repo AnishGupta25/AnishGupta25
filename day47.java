a55f726026be75021bf6fb0867d487e98874246c
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-20 20:00:21 +0530

    done

--- day47.java
+++ day47.java
@@ -79,4 +79,15 @@ public class day47 {
           }
         }
     }
+
+    public static void combinations(int cb, int tb, int ssf, int ts, String asf) {
+        if(cb > tb){
+            if(ssf == ts)
+            System.out.println(asf);
+            return;
+        }
+        
+        combinations(cb + 1 , tb , ssf + 1 , ts , asf + "i");
+        combinations(cb + 1 , tb , ssf + 0 , ts , asf + "-");
+    }
 }
