23234fc052741601b063e4be5f216c0d6988928f
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-22 15:45:45 +0530

    done

--- day49.java
+++ day49.java
@@ -43,4 +43,26 @@ public class day49 {
             }
         }
     }
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int i, int j) {
+        if (qpsf == tq) {
+          for (int row = 0; row < chess.length; row++) {
+            for (int col = 0; col < chess.length; col++) {
+              System.out.print(chess[row][col] ? "q\t" : "-\t");
+            }
+            System.out.println();
+          }
+          System.out.println();
+          return;
+        }
+    
+        for (int k = i; k < tq * tq; k++) {
+          int row = k / tq, col = k % tq;
+          if (!chess[row][col]) {
+            chess[row][col] = true;
+            queensCombinations(qpsf + 1, tq, chess , k , j);
+            chess[row][col] = false;
+          }
+        }
+    }
 }
