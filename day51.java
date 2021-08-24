96959f21f9e359056efef62dbf48c0d364db405a
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-24 16:20:15 +0530

    done

--- day51.java
+++ day51.java
@@ -69,4 +69,16 @@ public class day51 {
         }
         return ans;
     }
+
+    public ArrayList<Integer> findDisappearedNumbers2(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] arr = new int[nums.length +1];
+        for(int i = 0; i < nums.length; i++){
+            arr[nums[i]]++;
+        }
+        for(int i = 1; i <= nums.length; i++){
+            if(arr[i] < 1) ans.add(i);
+        }
+        return ans;
+    }
 }
