f7743bc6d6d36a0409e6f2db560e5b1b161d09f5
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-25 18:50:13 +0530

    done

--- day52.java
+++ day52.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Stack;
 public class day52 {
     public boolean judgeSquareSum(int c) {
         int l = 0, r = (int)Math.sqrt(c);
@@ -8,5 +10,25 @@ public class day52 {
             else return true;
         }
         return false;
+    }
+
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        Stack<Integer> st = new Stack<>();
+        int[] arr = new int[list.size()];
+        
+        for(int i = 0; i < list.size(); i++){
+            while(!st.isEmpty() && list.get(st.peek()) < list.get(i)){
+                arr[st.pop()] = list.get(i);
+            }
+            st.push(i);
+        }
+        return arr;
     }
 }
