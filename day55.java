5ad926cd7440c63d3a141a697c16fe5eda546e13
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-28 18:11:13 +0530

    done

--- day55.java
+++ day55.java
@@ -22,4 +22,20 @@ public class day55 {
             set2.remove(set2.size() - 1);
         }
     }
+
+    static int knapSack(int W, int wt[], int val[], int n) { 
+        int[][] dp = new int[n+1][W+1];
+        
+        for(int i = 1; i <= n; i++){
+            int wgt = wt[i - 1];
+            int v = val[i - 1];
+            for(int j = 1; j <= W; j++){
+                dp[i][j] = dp[i - 1][j];
+                if(wgt <= j){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - wgt] + v);
+                }
+            }
+        }
+        return dp[n][W];
+    } 
 }
