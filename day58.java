fa378624a7d79eccb8e6ee98407eb530bcf80452
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-31 21:43:56 +0530

    done

--- day58.java
+++ day58.java
@@ -30,4 +30,20 @@ public class day58 {
         }
         return val;
     }
+
+    int getMinDiff(int[] arr, int n, int k) {
+        Arrays.sort(arr);
+        
+        int ans = arr[n-1] - arr[0];
+        
+        int small = arr[0] + k;
+        int large = arr[n-1] - k;
+        
+        for(int i = 0; i < n-1; i++){
+            int min = Math.min(small , arr[i+1] - k);
+            int max = Math.max(large , arr[i] + k);
+            ans = Math.min(ans , max - min);
+        }
+        return ans;
+    }
 }
