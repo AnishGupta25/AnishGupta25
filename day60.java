0b8e5b51d4bd3ec9418636c7daa3ff80fe902812
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-02 01:14:27 +0530

    done

--- day60.java
+++ day60.java
@@ -5,7 +5,17 @@ public class day60 {
             double a = (double)((1.0 * N - (L * (L + 1)) / 2) / (L + 1));
             if (a - (int)a == 0.0) count++;
         }
-        
         return count;
+    }
+
+    public static void coinChange(int i, int[] coins, int amtsf, int tamt, String asf) {
+        if (i == coins.length) {
+          if (amtsf == tamt) {
+            System.out.println(asf + ".");
+          }
+          return;
+        }
+        coinChange(i + 1, coins, amtsf + coins[i], tamt, asf + coins[i] + "-");
+        coinChange(i + 1, coins, amtsf, tamt, asf + "");
     }
 }
