c5d713826ef0ed234f572ebec60e60bc2af8edd2
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-04 19:25:15 +0530

    done

--- day62.java
+++ day62.java
@@ -3,4 +3,29 @@ public class day62 {
         if(n == 1) return 0;
         return (solution(n - 1 , k) + k) % n;
     }
+    
+    static int counter = 1;
+    public static void solution(int i, int n, boolean[] used, String asf) {
+        if (i > n) {
+          System.out.println(counter + "." + asf);
+          counter++;
+          return;
+        }
+        if (used[i]) {
+          solution(i + 1, n, used, asf);
+        }
+        else {
+          used[i] = true;
+          solution(i + 1, n, used, asf + "(" + i + ") ");
+    
+          for (int j = i + 1; j <= n; j++) {
+            if (!used[j]) {
+              used[j] = true;
+              solution(i + 1, n, used, asf + "(" + i + "," + j + ") ");
+              used[j] = false;
+            }
+          }
+          used[i] = false;
+        }
+    }
 }
