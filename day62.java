3301bda6ef835f92723e72e85f072ab0b140a202
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-04 20:44:44 +0530

    done

--- day62.java
+++ day62.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class day62 {
     public static int solution(int n, int k) {
         if(n == 1) return 0;
@@ -28,4 +29,35 @@ public class day62 {
           used[i] = false;
         }
     }
+
+  static int max = 0;
+  public static void getMaxGold(int[][] arr) {
+    boolean[][] visited = new boolean[arr.length][arr[0].length];
+    for(int i = 0; i < arr.length; i++){
+        for(int j = 0; j < arr[0].length; j++){
+            if(arr[i][j] != 0 && !visited[i][j]){
+                ArrayList<Integer> bag = new ArrayList<>();
+                getMaxGoldHelper(arr , i , j , visited , bag);
+                int sum = 0;
+                for(int k : bag){
+                    sum += k;
+                }
+                max = Math.max(max , sum);
+            }
+        }
+    }
+  }
+  public static void getMaxGoldHelper(int[][] arr, int i, int j, boolean[][] visited, ArrayList<Integer> bag){
+    if (i < 0  || j < 0 || i >= arr.length || j >= arr[0].length || arr[i][j] == 0 || visited[i][j] == true) {
+    return;
+  }
+    
+    bag.add(arr[i][j]);
+    visited[i][j] = true;
+    
+    getMaxGoldHelper(arr, i - 1, j, visited, bag);
+    getMaxGoldHelper(arr, i , j + 1, visited, bag);
+    getMaxGoldHelper(arr, i , j - 1, visited, bag);
+    getMaxGoldHelper(arr, i + 1, j, visited, bag);
+  }
 }
