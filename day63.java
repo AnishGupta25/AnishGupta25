48b622986ffe68f9aa0515fafc16ab7b9fd05c96
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-05 01:34:18 +0530

    done

--- day63.java
+++ day63.java
@@ -20,4 +20,22 @@ public class day63 {
 	    }
 	    return count;
 	}
+
+	public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) return false;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : hand){
+            pq.add(i);
+        }
+        
+        while(!pq.isEmpty()){
+            int head = pq.poll();
+            
+            for(int i = 1; i < groupSize; i++){
+                if(!pq.remove(head+i)) return false;
+            }
+        }
+        return true;
+    }
 }
