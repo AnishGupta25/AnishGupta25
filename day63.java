3cc287a42662a6aee3fb60b9184108d5f4b3ee41
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-05 22:27:17 +0530

    done

--- day63.java
+++ day63.java
@@ -38,4 +38,43 @@ public class day63 {
         }
         return true;
     }
+
+	public static void solution(String unique, int idx, HashMap<Character, Integer> charIntMap, boolean[] usedNumbers, String s1, String s2, String s3) {
+		if(idx == unique.length()){
+			int num1 = getNumber(s1 , charIntMap);
+			int num2 = getNumber(s2 , charIntMap);
+			int num3 = getNumber(s3 , charIntMap);
+			
+			if(num1 + num2 == num3){
+				for(int i = 0; i < 26; i++){
+					char ch = (char)(i + 'a');
+					if(charIntMap.containsKey(ch)){
+						System.out.print(ch+"-"+charIntMap.get(ch) +" ");
+					}
+				}
+				System.out.println();
+			}
+			return;
+		}
+		char ch = unique.charAt(idx);
+		for(int i = 0; i < 10; i++){
+			if(!usedNumbers[i]){
+				usedNumbers[i] = true;
+				charIntMap.put(ch , i);
+				solution(unique, idx+1, charIntMap, usedNumbers, s1, s2, s3);
+				usedNumbers[i] = false;
+				charIntMap.remove(ch);
+			}
+		}
+	}
+	  
+	public static int getNumber(String str , HashMap<Character, Integer> charIntMap){
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < str.length();i++){
+			char ch = str.charAt(i);
+			sb.append(charIntMap.get(ch));
+		}
+		return Integer.parseInt(sb.toString());
+	}
 }
