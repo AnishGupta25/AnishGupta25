b7dfd17ff9835f98fa30c9ed07ab7a62bc992c1a
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-07 23:24:20 +0530

    done

--- day65.java
+++ day65.java
@@ -59,4 +59,14 @@ public class day65 {
             else st.push(x);
         }	
     }
+
+    public boolean isThree(int n) {
+        int count = 2;
+        
+        for(int i = 2; i < n-1; i++){
+            if(n % i == 0) count++;
+        }
+        if(count == 3) return true;
+        return false;
+    }
 }
