45d4dcdd0740158646ad42d85219f74f19b8228d
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-08 01:54:08 +0530

    done

--- day66.java
+++ day66.java
@@ -5,4 +5,25 @@ public class day66 {
         }
         return nums;
     }
+
+    class Node{
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+
+    public static boolean detectLoop(Node head){
+        if(head == null) return false;
+        
+        Node fast = head;
+        Node slow = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow) return true;
+        }
+        return false;
+    }
 }
