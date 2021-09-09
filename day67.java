0e27a695c235583eb69d1e9d59c3d742a74947d8
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-09 19:34:56 +0530

    done

--- day67.java
+++ day67.java
@@ -16,4 +16,52 @@ public class day67 {
         }
         return str.toString();
     }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = middleNode(head);
+        ListNode rev = reverse(temp);
+        
+        while(rev != null){
+            if(head.val != rev.val) return false;
+            
+            head = head.next;
+            rev = rev.next;
+        }
+        return true;
+    }
+    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null) return head;
+        
+        ListNode prev = null;
+        ListNode cur = head;
+        
+        while(cur != null){
+            ListNode fwd = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = fwd;
+        }
+        return prev;
+    }
+    
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 }
