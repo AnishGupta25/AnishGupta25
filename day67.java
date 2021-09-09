995d698f0458ba5f1d4d4554a0e5c6cb23449d92
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-09 22:42:26 +0530

    done

--- day67.java
+++ day67.java
@@ -52,7 +52,7 @@ public class day67 {
         return prev;
     }
     
-    public ListNode middleNode(ListNode head) {
+    public ListNode middleNode1(ListNode head) {
 
         ListNode slow = head;
         ListNode fast = head;
@@ -90,5 +90,49 @@ public class day67 {
             slow = slow.next;
         }
         return slow;
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+    
+        while (fast.next != null && fast.next.next != null) {
+          slow = slow.next;
+          fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return l1 != null ? l1 : l2;
+        ListNode res = null;
+        ListNode r = res;
+        while (l1 != null && l2 != null) {
+          if (l1.val <= l2.val) {
+            r.next = l1;
+            l1 = l1.next;
+            r = r.next;
+          }
+          else if (l2.val < l1.val) {
+            r.next = l2;
+            l2 = l2.next;
+            r = r.next;
+          }
+        }
+        if (l1 != null) r.next = l1;
+        if (l2 != null) r.next = l2;
+    
+        return res.next;
+      }
+
+    public static ListNode mergeSort(ListNode head) {
+        if (head.next == null) return head;
+        
+        ListNode mid = middleNode(head);
+        ListNode rh = mid.next;
+        mid.next = null;
+        
+        return mergeTwoLists(mergeSort(head) , mergeSort(rh));
     }
 }
