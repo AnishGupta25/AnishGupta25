6c448ca9580739c40f90a9dcfa7167e5220abdab
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-10 20:08:07 +0530

    done

--- day68.java
+++ day68.java
@@ -23,4 +23,15 @@ public class day68 {
         }
         return start;
     }
+
+    public int searchInsert(int[] nums, int target) {
+        int i = 0 , j = nums.length - 1;
+        while(i <= j){
+            int mid = i + (j - i) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) j = mid - 1;
+            else i = mid + 1;
+        }
+        return i;
+    }
 }
