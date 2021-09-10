9e01c2c73093a6ab9476d60f5e6bbf07f73b96a3
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-10 20:00:10 +0530

    done

--- day68.java
+++ day68.java
@@ -11,4 +11,16 @@ public class day68 {
         }
         return -1;
     }
+
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isBadVersion(mid)) end = mid-1;
+            else start = mid+1;
+        }
+        return start;
+    }
 }
