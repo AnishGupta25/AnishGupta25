32fdb6459b9ab1257b55c448775c9a9d88b56b28
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-11 21:18:55 +0530

    done

--- day69.java
+++ day69.java
@@ -68,4 +68,22 @@ public class day69 {
         }
         return res;
     }
+
+    class Node{
+        int data;
+        Node left,right;
+        Node(int d){
+            data=d;
+            left=right=null;
+        }
+    }
+
+    boolean isIdentical(Node root1, Node root2){
+	    if(root1 == null && root2 == null) return true;
+	    
+	    if(root1 != null && root2 != null){
+	        return root1.data == root2.data && isIdentical(root1.left , root2.left) && isIdentical(root1.right , root2.right);
+	    }
+	    return false;
+	}
 }
