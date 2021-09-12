dfb3298c45c654654decb42b2a702d5e242d64bb
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-12 12:58:49 +0530

    done

--- day70.java
+++ day70.java
@@ -16,4 +16,21 @@ public class day70 {
             }
         }
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        
+        int i = 0, j = nums.length - 1;
+        while(i < j){
+            int sum = nums[i] + nums[j];
+            if(sum == target){
+                ans[0] = i + 1;
+                ans[1] = j + 1;
+                break;
+            }
+            else if(sum > target) j--;
+            else i++;
+        }
+        return ans;
+    }
 }
