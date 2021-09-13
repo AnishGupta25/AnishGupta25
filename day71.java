ca391ad8876329a47fc1a2c08e3d5a590604a9ba
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-13 13:05:13 +0530

    done

--- day71.java
+++ day71.java
@@ -27,4 +27,13 @@ public class day71 {
         }
         return sb.toString();
     }
+
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
 }
