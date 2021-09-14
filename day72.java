1ea72bf04d47b072ad7462932bcd77cec7c59c5e
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-14 12:30:32 +0530

    done

--- day72.java
+++ day72.java
@@ -56,4 +56,35 @@ public class day72 {
         sumOfLeftLeavesHelper(root.left , -1);
         sumOfLeftLeavesHelper(root.right , 1);
     }
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode res = dummy;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                res.next = l1;
+                res = res.next;
+                l1 = l1.next;
+            }
+            else if(l2.val < l1.val){
+                res.next = l2;
+                res = res.next;
+                l2 = l2.next;
+            }
+        }
+        
+        if(l1 != null) res.next = l1;
+        if(l2 != null) res.next = l2;
+        return dummy.next;
+    }
 }
