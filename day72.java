2090f5d3991f59de0fbef32bb3fc0a6e897a39ea
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-14 16:14:15 +0530

    done

--- day72.java
+++ day72.java
@@ -87,4 +87,22 @@ public class day72 {
         if(l2 != null) res.next = l2;
         return dummy.next;
     }
+
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length < 3) return false;
+        
+        int i = Integer.MAX_VALUE;
+        int j = Integer.MAX_VALUE;
+        
+        for(int num : nums){
+            if(num <= i){
+                i = num;
+            }
+            else if(num <= j){
+                j = num;
+            }
+            else return true;
+        }
+        return false;
+    }
 }
