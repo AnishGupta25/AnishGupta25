6281ca779f16b830c2d54c28527a71045d8670b0
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-15 14:45:52 +0530

    done

--- day73.java
+++ day73.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class day73 {
     public static void sort012(int a[], int n){
         int i = 0 , j = 0 , k = n - 1;
@@ -21,5 +22,38 @@ public class day73 {
                 k--;
             }
         }
+    }
+    public int lengthOfLongestSubstring(String s) {
+        int i = -1 , j = -1;
+        int ans = 0;
+        HashMap<Character , Integer> map = new HashMap<>();
+        
+        while(true){
+            boolean flag1 = false;
+            boolean flag2 = false;
+            while(i < s.length() - 1){
+                flag1 = true;
+                i++;
+                char ch = s.charAt(i);
+                map.put(ch , map.getOrDefault(ch , 0) + 1);
+                
+                if(map.get(ch) > 1) break;
+                else {
+                    int len = i - j;
+                    if(len > ans) ans = len;
+                }
+            }
+            
+            while(j < i){
+                flag2 = true;
+                j++;
+                char ch = s.charAt(j);
+                map.put(ch , map.get(ch) - 1);
+                
+                if(map.get(ch) == 1) break;
+            }
+            if(!flag1 && !flag2) break;
+        }
+        return ans;
     }
 }
