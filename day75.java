41973bf86396e7cb928e23387334f9af8466cdfb
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-17 23:09:03 +0530

    done

--- day75.java
+++ day75.java
@@ -66,4 +66,11 @@ public class day75 {
         }
         return arr;
     }
+
+    public boolean isPowerOfTwo(int n) {
+        if(n==0) return(false);
+        if(n==1) return(true);
+        if(n%2==0) return(isPowerOfTwo(n/2));
+        else return false;
+    }
 }
