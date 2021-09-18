641badd43922933e3b0be1370993fbd15fcb6475
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-18 19:37:00 +0530

    done

--- day76.java
+++ day76.java
@@ -78,4 +78,14 @@ public class day76 {
         }
         return forg == 0 ? ans : -1;
     }
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        int dx = coordinates[1][0]-coordinates[0][0];
+        int dy = coordinates[1][1]-coordinates[0][1];
+
+        for(int i =1; i<coordinates.length-1; i++)
+            if(dx*(coordinates[i+1][1]-coordinates[i][1]) != dy*(coordinates[i+1][0]-coordinates[i][0])) return false;
+
+        return true;
+    }
 }
