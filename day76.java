2ce59168f5163984435ecd9f0a671cfdc52c06f2
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-18 16:41:47 +0530

    done

--- day76.java
+++ day76.java
@@ -38,4 +38,44 @@ public class day76 {
             this.y = y;
         }
     }
+
+    class Pair1{
+        int time;
+        int x , y;
+        Pair1(int time , int x , int y){
+            this.time = time;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public int orangesRotting(int[][] grid) {
+        int forg = 0;
+        Queue<Pair1> qu = new ArrayDeque<>();
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2) qu.add(new Pair1(0 , i , j));
+                if(grid[i][j] == 1) forg++;
+            }
+        }
+        
+        int ans = 0;
+        
+        while(qu.size() > 0){
+            Pair1 temp = qu.remove();
+            
+            for(int i = 0; i < dirs.length; i++){
+                int row = temp.x + dirs[i][0];
+                int col = temp.y + dirs[i][1];
+                if(row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == 1){
+                    grid[row][col] = 2;
+                    qu.add(new Pair1(temp.time + 1 , row , col));
+                    forg--;
+                    ans = temp.time + 1;
+                }
+            }
+        }
+        return forg == 0 ? ans : -1;
+    }
 }
