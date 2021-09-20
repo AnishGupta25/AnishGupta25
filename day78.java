7115f5ae99d00ef0711c6e7bef4ebbdc723be43c
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-20 22:34:52 +0530

    done

--- day78.java
+++ day78.java
@@ -70,4 +70,25 @@ public class day78 {
         Permutation(list , str , sb , idx+1);
         sb.deleteCharAt(sb.length()-1);
     }
+
+    static ArrayList<Integer> factorial(int N){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        
+        for(int i = 1; i <= N; i++){
+            int carry = 0;
+            for(int j = 0; j < list.size(); j++){
+                int res = list.get(j) * i + carry;
+                carry = res / 10;
+                list.set(j , res % 10);
+            }
+            while(carry != 0){
+                list.add(carry%10);
+                carry /= 10;
+            }
+        }
+        
+        Collections.reverse(list);
+        return list;
+    }
 }
