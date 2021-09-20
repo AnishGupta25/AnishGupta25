b0aaafca76c8195e8fe89931d56e5ee35ba0fbdf
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-21 00:35:58 +0530

    done

--- day79.java
+++ day79.java
@@ -26,4 +26,25 @@ public class day79 {
         }
         return i == m && j == n;
     }
+
+    static int maxCandy(int height[], int n) {
+	    if(n <= 2) return 0;
+	    
+	    int i = 0, j = n - 1;
+	    int max = 0;
+	    while(i < j){
+	        int val1 = height[i];
+	        int val2 = height[j];
+	        
+	        int temp = Math.min(val1 , val2) * (j - i - 1);
+	        max = Math.max(max , temp);
+	        if(val1 < val2) i++;
+	        else if(val1 > val2) j--;
+	        else{
+	            i++;
+	            j--;
+	        }
+	    }
+	    return max;
+	}
 }
