30c811ac30785bab425ec4c6c912118bd76b7bdf
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-20 17:36:53 +0530

    done

--- day78.java
+++ day78.java
@@ -8,7 +8,7 @@ public class day78 {
     
     public void Combination(ArrayList<ArrayList<Integer>> list , ArrayList<Integer> comb , int n , int k , int start){
         if(k == 0){
-            list.add(new ArrayList(comb));
+            list.add(new ArrayList<>(comb));
             return;
         }
         
@@ -16,6 +16,27 @@ public class day78 {
             comb.add(i);
             Combination(list , comb , n , k - 1 , i+1);
             comb.remove(comb.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Permutations(list , new ArrayList<Integer>() , nums);
+        return list;
+    }
+    
+    public void Permutations(List<List<Integer>> list , ArrayList<Integer> permu , int[] nums){
+        if(permu.size() == nums.length){
+            list.add(new ArrayList<>(permu));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!permu.contains(nums[i])){
+                permu.add(nums[i]);
+                Permutations(list , permu , nums);
+                permu.remove(permu.size() - 1);
+            }
         }
     }
 }
