4ea400443a1ded89a899615df4222c35a3103f3a
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-21 14:14:36 +0530

    done

--- day79.java
+++ day79.java
@@ -47,4 +47,18 @@ public class day79 {
 	    }
 	    return max;
 	}
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        else if(n == 2) return Math.max(nums[0] , nums[1]);
+        else{
+            nums[2] = Math.max(nums[0] + nums[2] , nums[1]);
+            for(int i = 3; i < n; i++){
+                nums[i] = nums[i] + Math.max(nums[i - 2] , nums[i - 3]);
+            }
+        }
+        
+        return Math.max(nums[n-1] , nums[n-2]);
+    }
 }
