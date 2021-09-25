cd2e76603c6317afc92bd0698c2f5ffe2c94b30b
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-25 22:52:23 +0530

    done

---
+++ day83.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class day83 {
+    int maxLen(int arr[], int n){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int sum = 0; 
+        int max_len = 0;
+        
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            
+            if(arr[i] == 0 && max_len == 0) max_len = 1;
+            
+            if(sum == 0) max_len = i + 1;
+            
+            Integer prev_idx = hm.get(sum);
+            
+            if(prev_idx != null) max_len = Math.max(max_len , i - prev_idx);
+            else hm.put(sum , i);
+        }
+        return max_len;
+    }
+}
