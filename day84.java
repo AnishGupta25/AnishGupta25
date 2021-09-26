92c875a2b12b1a8515350cfff8ca974f36d77f7e
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-26 19:09:11 +0530

    done

--- day84.java
+++ day84.java
@@ -18,4 +18,43 @@ public class day84 {
         }
         return count;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        TreeNode root = BuiltHelper(preorder , inorder , 0 , n-1 , 0 , n-1);
+        return root;
+    }
+    
+    public TreeNode BuiltHelper(int[] preorder, int[] inorder , int ps , int pe , int is , int ie){
+        if(ps > pe) return null;
+        
+        TreeNode node = new TreeNode(preorder[ps]);
+        
+        int k = -1;
+        for(int i = is; i <= ie; i++){
+            if(inorder[i] == node.val){
+                k = i;
+                break;
+            }
+        }
+        int cls = k - is;
+        
+        node.left = BuiltHelper(preorder , inorder , ps+1 , ps+cls , is , k-1);
+        node.right = BuiltHelper(preorder , inorder , ps+cls+1 , pe , k+1 , ie);
+        
+        return node;
+    }
 }
