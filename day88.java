2a0215dbd2f591a9ba44c870a5a63d40609c5bb5
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-30 17:00:19 +0530

    done

--- day88.java
+++ day88.java
@@ -1,6 +1,26 @@
+import java.util.*;
 public class day88 {
     static int candies(int m, int n){
         int x = (n * m) - n - m;
         return (x / 2) + 1;
+    }
+
+    public List<Integer> pathInZigZagTree(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        int clvl = 0 , llvl = 1;
+        while(clvl < n){
+            clvl += llvl;
+            llvl *= 2;
+        }
+        llvl /= 2;
+        
+        while(n != 0){
+            list.add(n);
+            n = (3 * llvl - n - 1) / 2;
+            llvl /= 2;
+        }
+        Collections.reverse(list);
+        return list;
     }
 }
