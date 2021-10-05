5a8e253b4a1cc928b34fc2a7ffbe5013a24a622e
AnishGupta25 <anishgupta2522@gmail.com>
2021-10-05 21:30:31 +0530

    done

---
+++ day93.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class day93 {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        
+        dp[0] = nums[0];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((int a[], int b[]) -> b[0] - a[0]);
+        pq.add(new int[] {nums[0] , 0});
+        
+        for(int i = 1; i < nums.length; i++){
+            while(!pq.isEmpty() && pq.peek()[1] < i-k) pq.remove();
+            
+            dp[i] = nums[i] + pq.peek()[0];
+            pq.add(new int[]{dp[i], i});
+        }
+        return dp[n-1];
+    }
+}
