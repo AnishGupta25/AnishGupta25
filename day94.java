7334470b467418b3a9f677592eb6ce79e1d7fd1b
AnishGupta25 <anishgupta2522@gmail.com>
2021-10-06 12:53:47 +0530

    done

---
+++ day94.java
@@ -0,0 +1,16 @@
+public class day94 {
+    public int editDistance(String s, String t) {
+        int n = s.length() , m = t.length();
+        
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <=m; j++){
+                if(i == 0) dp[i][j] = j;
+                else if(j == 0) dp[i][j] = i;
+                else if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + Math.min(dp[i-1][j-1] , Math.min(dp[i][j-1] , dp[i-1][j]));
+            }
+        }
+        return dp[n][m];
+    }
+}
