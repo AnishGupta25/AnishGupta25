40fb16393f99efaf53cee583dc3f299c4eac57eb
AnishGupta25 <anishgupta2522@gmail.com>
2021-10-08 23:26:10 +0530

    done

--- day96.java
+++ day96.java
@@ -2,4 +2,18 @@ public class day96 {
     public boolean canWinNim(int n) {
         return n % 4 != 0;
     }
+
+    public char nextGreatestLetter(char[] let, char tar) {
+        if(tar >= let[let.length - 1]) return let[0];
+        
+        int i = 0 , j = let.length - 1;
+        
+        while(i <= j){
+            int mid = i + (j-i) / 2;
+            
+            if(let[mid] <= tar) i = mid + 1;
+            else j =  mid - 1;
+        }
+        return let[i];
+    }
 }
