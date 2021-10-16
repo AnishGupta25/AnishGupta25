8caf5c3cb835362c5436615a2c105f314b00a7f2
AnishGupta25 <anishgupta2522@gmail.com>
2021-10-16 22:02:46 +0530

    done

--- day104.java
+++ day104.java
@@ -91,4 +91,29 @@ public class day104 {
         
         treetoll(root.right);
     }
+
+    public int rob(TreeNode root) {
+        Pair res = robHelper(root);
+        return Math.max(res.inc , res.exc);
+    }
+    
+    public Pair robHelper(TreeNode root){
+        if(root == null) return new Pair(0 , 0);
+        
+        Pair lp = robHelper(root.left);
+        Pair rp = robHelper(root.right);
+        
+        int inc = root.val + lp.exc + rp.exc;
+        int exc = Math.max(lp.inc , lp.exc) + Math.max(rp.inc , rp.exc);
+        
+        return new Pair(inc , exc);
+    }
+    
+    class Pair{
+        int inc , exc;
+        Pair(int inc , int exc){
+            this.inc = inc;
+            this.exc = exc;
+        }
+    }
 }
