fd5aba90c198e0eac826a1792a5d6b2d1e43e3c0
AnishGupta25 <anishgupta2522@gmail.com>
2021-11-20 22:26:12 +0530

    done

--- nov20.java
+++ nov20.java
@@ -56,4 +56,15 @@ public class nov20 {
         }
         return result;
     }
+
+    public boolean searchMatrix(int[][] mat, int target) {
+        int row = 0 , col = mat[0].length - 1;
+        
+        while(row < mat.length && col >= 0){
+            if(mat[row][col] == target) return true;
+            else if(mat[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
 }
