313491df84473623ea53f5f82d5fd89ac6623050
AnishGupta25 <anishgupta2522@gmail.com>
2021-11-21 13:13:08 +0530

    done

--- nov21.java
+++ nov21.java
@@ -20,4 +20,19 @@ public class nov21 {
         }
         return true;
     }
+
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                if(nums[0] != nums[i + 1]) return nums[0];
+            }
+            else if(i == nums.length - 1){
+                if(nums[i] != nums[i - 1]) return nums[i];
+            }
+            else if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) return nums[i];
+        }
+        return 0;
+    }
 }
