9eec41d395233c1a80004e57eb6d2a08feb27bda
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-01 19:21:20 +0530

    done

--- dec1.java
+++ dec1.java
@@ -38,4 +38,19 @@ public class dec1 {
         }
     }
     
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(int i = 0; i < magazine.length(); i++){
+            map.put(magazine.charAt(i) , map.getOrDefault(magazine.charAt(i) , 0) + 1);
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(!map.containsKey(ransomNote.charAt(i))) return false;
+            
+            map.put(ransomNote.charAt(i) , map.get(ransomNote.charAt(i)) - 1);
+            if(map.get(ransomNote.charAt(i)) < 0) return false;
+        }
+        return true;
+    }
 }
