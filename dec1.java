b0982ded027d11ab9f1a898831a31b042cdfe4ca
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-01 19:07:47 +0530

    done

--- dec1.java
+++ dec1.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class dec1 {
     public int firstUniqChar(String str) {
         if(str.length() == 1) return 0;
@@ -13,4 +14,28 @@ public class dec1 {
         }
         return -1;
     }
+
+    class MyQueue {
+        Stack<Integer> st;
+        public MyQueue() {
+            st = new Stack<>();
+        }
+        
+        public void push(int x) {
+            st.add(0 , x);
+        }
+        
+        public int pop() {
+            return st.pop();
+        }
+        
+        public int peek() {
+            return st.peek();
+        }
+        
+        public boolean empty() {
+            return st.isEmpty();
+        }
+    }
+    
 }
