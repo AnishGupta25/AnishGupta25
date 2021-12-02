49a9e32937472749dd3f06e9e251fa5bf60150e1
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-02 19:55:23 +0530

    done

--- dec2.java
+++ dec2.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class dec2 {
     class ListNode{
         int val;
@@ -20,5 +21,22 @@ public class dec2 {
         }
         odd.next = even;
         return head;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(pq.size() < k){
+                pq.add(nums[i]);
+            }
+            else{
+                if(pq.peek() < nums[i]){
+                    pq.poll();
+                    pq.add(nums[i]);
+                }
+            }
+        }
+        return pq.peek();
     }
 }
