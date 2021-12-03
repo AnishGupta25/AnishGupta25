6221481ced6b9f081ca332e3edfde1b801ec9769
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-03 10:52:58 +0530

    done

---
+++ dec3.java
@@ -0,0 +1,18 @@
+public class dec3 {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        
+        int l = 1, r = 1, ans = nums[0];
+        
+        for(int i = 0; i < n; i++){
+            l = l == 0 ? 1 : l;
+            r = r == 0 ? 1 : r;
+            
+            l *= nums[i];
+            r *= nums[n - i - 1];
+            
+            ans = Math.max(ans , Math.max(r , l));
+        }
+        return ans;
+    }
+}
