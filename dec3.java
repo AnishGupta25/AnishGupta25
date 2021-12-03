19d9dac2f03fbeb8e681e68aeae50717684dacf6
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-03 23:22:16 +0530

    done

--- dec3.java
+++ dec3.java
@@ -44,4 +44,28 @@ public class dec3 {
         }
         return arr;
     }
+
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        
+        for(int i = 0; i < n; i++){
+            int l = 0, r = n-1;
+            
+            while(l < r){
+                int temp = mat[i][l];
+                mat[i][l] = mat[i][r];
+                mat[i][r] = temp;
+                l++;
+                r--;
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[n - j -1][n - i - 1];
+                mat[n - j -1][n - i - 1] = temp;
+            }
+        }
+    }
 }
