7d61468d1b1569ed0ae24b2afdf60c6ef37371bf
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-12 22:20:45 +0530

    done

---
+++ dec12.java
@@ -0,0 +1,21 @@
+public class dec12 {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int i : nums) sum += i;
+        
+        if( sum % 2 != 0) return false;
+        
+        sum /= 2;
+        
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for(int i : nums){
+            for(int j = sum; j > 0; j--){
+                if(j >= i){
+                    dp[j] = dp[j] || dp[j - i];
+                }
+            }
+        }
+        return dp[sum];
+    }
+}
