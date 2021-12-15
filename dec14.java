be1596cf0b87b137e8b894ad1fba5d2a5f50fc83
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-15 23:56:22 +0530

    done

--- dec14.java
+++ dec14.java
@@ -4,14 +4,14 @@ public class dec14 {
         TreeNode right;
         TreeNode left;
     }
-    int sum = 0;
+    int sum1 = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null) return 0;
         
         int lp = rangeSumBST(root.left , low , high);
         int rp = rangeSumBST(root.right , low , high);
         
-        if(root.val <= high && root.val >= low) sum += root.val;
-        return sum;
+        if(root.val <= high && root.val >= low) sum1 += root.val;
+        return sum1;
     }
 }
