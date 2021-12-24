742291103e6940affff91ea594a6b18fccc130fd
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-24 09:17:31 +0530

    done

---
+++ dec24.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class dec24 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals , (a , b) -> Integer.compare(a[0] , b[0]));
+        
+        ArrayList<int[]> ans = new ArrayList<>();
+        
+        for(int[] temp : intervals){
+            if(ans.isEmpty() || ans.get(ans.size() - 1)[1] < temp[0]) ans.add(temp);
+            else ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1] , temp[1]);
+        }
+         return ans.toArray(new int[ans.size()][]);
+    }
+}
