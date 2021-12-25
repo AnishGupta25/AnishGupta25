28ad8c2c4b3e7b0f79be34addbb326f021ce8991
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-25 21:12:17 +0530

    done

--- dec24.java
+++ dec24.java
@@ -8,6 +8,7 @@ public class dec24 {
         for(int[] temp : intervals){
             if(ans.isEmpty() || ans.get(ans.size() - 1)[1] < temp[0]) ans.add(temp);
             else ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1] , temp[1]);
+            
         }
          return ans.toArray(new int[ans.size()][]);
     }
