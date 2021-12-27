8aada0fd02b0ee6ee83a3088aab5e5f3eafa843a
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-27 10:30:08 +0530

    done

--- dec27.java
+++ dec27.java
@@ -9,4 +9,26 @@ public class dec27 {
         }
         return num;
     }
+
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1 && nums[0] == val) return 0;
+        
+        int l = 0 , r = nums.length - 1 , count = 0;
+        
+        while(l <= r){
+            if(nums[l] == val){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                
+                r--;
+            }
+            else{
+                l++;
+                count++;
+            }
+        }
+        return count;
+    }
 }
