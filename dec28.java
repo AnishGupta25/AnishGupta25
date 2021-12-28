daf4c17b5c180a3304f882130893996c495570d8
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-28 10:34:38 +0530

    done

--- dec28.java
+++ dec28.java
@@ -2,6 +2,9 @@ public class dec28 {
     class ListNode{
         ListNode next;
         int val;
+        ListNode(int val){
+            this.val = val;
+        }
     }
     public ListNode middleNode(ListNode head) {
         if(head == null || head.next == null) return head;
@@ -14,5 +17,32 @@ public class dec28 {
             fast = fast.next.next;
         }
         return slow;
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode b_Head = new ListNode(0);
+        ListNode bHead = b_Head;
+        ListNode A_Head = new ListNode(0);
+        ListNode aHead = A_Head;
+        
+        while(head != null){
+            if(head.val < x){
+                bHead.next = head;
+                bHead = bHead.next;
+            }
+            else{
+                aHead.next = head;
+                aHead = aHead.next;
+            }
+            head = head.next;
+        }
+        
+        aHead.next = null;
+        
+        bHead.next = A_Head.next;
+        
+        return b_Head.next;
     }
 }
