be322cf98572bc0970e78c42ea8b2e2b50c329da
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-29 09:40:32 +0530

    done

--- dec29.java
+++ dec29.java
@@ -21,4 +21,15 @@ public class dec29 {
         helper(node.left);
         helper(node.right);
     }
+
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > nums[r]) l = mid + 1;
+            else if(nums[mid] < nums[r]) r = mid;
+        }
+        return nums[l];
+    }
 }
