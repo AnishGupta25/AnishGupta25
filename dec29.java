b6ef290ed6dd270259fe24f062b1444d9ad1d83f
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-29 09:50:41 +0530

    done

--- dec29.java
+++ dec29.java
@@ -32,4 +32,19 @@ public class dec29 {
         }
         return nums[l];
     }
+
+    public int findPeakElement(int[] nums) {
+        int l = 0, r =nums.length - 1;
+        
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            
+            if(mid == 0 || nums[mid] > nums[mid - 1]){
+                if(mid == nums.length - 1 || nums[mid + 1] < nums[mid]) return mid;
+                else l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+        return 0;
+    }
 }
