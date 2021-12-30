43fe4cfd3b61c22891773957195d1feb5a7e5c37
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-30 10:00:50 +0530

    done

--- dec30.java
+++ dec30.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class dec30 {
     class ListNode{
         ListNode next;
@@ -42,5 +43,33 @@ public class dec30 {
             ans++;
         }
         return ans;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i< nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            int a = nums[i];
+            int start = i+1, end = nums.length-1;
+            while(start < end){
+                int sum = a + nums[start] + nums[end];
+                if(sum > 0){
+                    end--;
+                }else if(sum < 0){
+                    start++;
+                }else{
+					result.add(Arrays.asList(a,nums[start],nums[end]));
+                    while(start< end && nums[start] == nums[start+1])
+                        start++;	
+                    while( start< end && nums[end] == nums[end-1])
+                        end--;
+                    start++;
+                    end--;
+                }
+            }
+        }
+        return result;
     }
 }
