a8046f0aee3a6d1c20416736e435e97a35d4622e
AnishGupta25 <anishgupta2522@gmail.com>
2021-12-31 09:45:45 +0530

    done

--- dec31.java
+++ dec31.java
@@ -20,4 +20,27 @@ public class dec31 {
         
         return Math.max(left , right);
     }
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int Area = 0;
+        
+        while(left < right){
+            int width = height[left] < height[right] ? height[left] : height[right];
+            int length = right - left;
+            int curArea = length * width;
+            
+            if(curArea > Area){
+                Area = curArea;
+            }
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return Area;
+    }
 }
