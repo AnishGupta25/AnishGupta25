f13b00f88e10da3fd5ebfe98b3a65c1f0dda8f03
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-02 13:36:26 +0530

    done

--- jan2.java
+++ jan2.java
@@ -25,4 +25,19 @@ public class jan2 {
         }
         return arr;
     }
+
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] arr = new int[60];
+        int ans = 0;
+        
+        for(int i = 0; i < time.length; i++){
+            arr[time[i] % 60]++;
+        }
+        
+        for(int i = 0; i <= 30; i++){
+            if(i == 0 || i == 30) ans += (arr[i] * (arr[i]-1))/2;
+            else ans += arr[i] * arr[60-i];
+        }
+        return ans;
+    }
 }
