9ea23571f38fa07c8cf400fcffafcca2120ae93f
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-08 13:40:49 +0530

    done

--- jan8.java
+++ jan8.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class jan8 {
     public int majorityElement(int[] nums) {
         int idx = 0 , freq = 1;
@@ -12,5 +13,36 @@ public class jan8 {
             }
         }
         return nums[idx];
+    }
+    public List<Integer> majorityElement2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int num1 = -1 , num2 = -1 , count1 = 0 , count2 = 0;
+        
+        for(int elem : nums){
+            if(elem == num1) count1++;
+            else if(elem == num2) count2++;
+            else if(count1 == 0){
+                num1 = elem;
+                count1 = 1;
+            } 
+            else if(count2 == 0){
+                num2 = elem;
+                count2 = 1;
+            } 
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for(int elem : nums){
+            if(elem == num1) count1++;
+            if(elem == num2) count2++;
+        }
+        if(count1 > nums.length / 3) res.add(num1);
+        if(count2 > nums.length / 3 && num1 != num2) res.add(num2);
+       
+        return res;
     }
 }
