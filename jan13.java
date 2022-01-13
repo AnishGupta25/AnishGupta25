fd0c4ea13a0294e5ca41d647739d5849e54fbd86
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-13 09:08:49 +0530

    done

---
+++ jan13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class jan13 {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points , (a , b) -> Integer.compare(a[1],b[1]));
+        
+        int arrows = 1;
+        int dia = points[0][1];
+        
+        for(int i = 1; i < points.length; i++){
+            if(points[i][0] > dia){
+                arrows += 1;
+                dia = points[i][1];
+            }
+        }
+        return arrows;
+    }
+}
