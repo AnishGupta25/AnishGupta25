28f416b0573c68e3e735765993559642e8d1b413
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-22 14:38:05 +0530

    done

--- jan22.java
+++ jan22.java
@@ -7,4 +7,48 @@ public class jan22 {
         }
         return ans;
     }
+
+    int m = 1000000007;
+    public int kConcatenationMaxSum(int[] arr, int k) {
+        int sum = Sum(arr);
+        if(k == 1) return Kadens1(arr);
+        int kdns = Kadens(arr);
+        if(sum > 0){
+            long ans = kdns + (long)(k - 2) * sum; 
+            return (int)(ans % m);
+        } 
+        return kdns;
+    }
+    
+    public int Kadens(int[] arr){
+        int max = 0;
+        int cmax = arr[0];
+        for(int i = 1; i < 2*arr.length; i++){
+            int idx = i % arr.length;
+            if(cmax > 0) cmax += arr[idx];
+            else cmax = arr[idx];
+            max = Math.max(cmax , max);
+        }
+        return max;  
+    }
+    public int Kadens1(int[] arr){
+        int max = arr[0];
+        int cmax = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            int idx = i % arr.length;
+            if(cmax > 0) cmax += arr[idx];
+            else cmax = arr[idx];
+            max = Math.max(cmax , max);
+        }
+        return max;
+        
+    }
+    
+    public int Sum(int[] arr){
+        int sum = 0;
+        for(int i : arr){
+            sum += i;
+        }
+        return sum;
+    }
 }
