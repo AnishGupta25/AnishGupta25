e5c8e6914c365b2e1e9512ea979a6497a7ceba5c
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-23 16:52:16 +0530

    done

--- jan23.java
+++ jan23.java
@@ -13,4 +13,47 @@ public class jan23 {
         if(num > high || i > 9) return;
         dfs(i+1 , low , high , num*10 + i);
     }
+    
+    public String pushDominoes(String dominoes) {
+        dominoes = "L" + dominoes + "R";
+        StringBuilder sb = new StringBuilder(dominoes);
+        
+        int i = 0;
+        while(i < dominoes.length() - 1){
+            int j = i+1;
+            
+            while(j < dominoes.length() && dominoes.charAt(j) == '.'){
+                j++;
+            }
+            
+            update(sb , i , j);
+            i = j;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(0);
+        
+        return sb.toString();
+    }
+    void update(StringBuilder sb , int i , int j){
+        if(sb.charAt(i) == 'L' && sb.charAt(j) == 'L'){
+            for(int n = i+1; n < j; n++){
+                sb.setCharAt(n , 'L');
+            }
+        }
+        else if(sb.charAt(i) == 'R' && sb.charAt(j) == 'R'){
+            for(int n = i+1; n < j; n++){
+                sb.setCharAt(n , 'R');
+            }
+        }
+        else if(sb.charAt(i) == 'R' && sb.charAt(j) == 'L'){
+            int n = i+1 , m = j-1;
+            while(n < m){
+                sb.setCharAt(n , 'R');
+                sb.setCharAt(m , 'L');
+                n++;
+                m--;
+            }
+        }
+        
+    }
 }
