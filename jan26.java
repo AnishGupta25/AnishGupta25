993b3df21e2d5ac8acf06cdcb9536c94e8a3d370
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-26 22:11:58 +0530

    done

---
+++ jan26.java
@@ -0,0 +1,45 @@
+public class jan26 {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer> t1 = new ArrayList<>();
+        List<Integer> t2 = new ArrayList<>();
+        
+        inorder(root1 , t1);
+        inorder(root2 , t2);
+        merge(t1 , t2 , res);
+        
+        return res;
+    }
+    
+    public void inorder(TreeNode root , List<Integer> list){
+        if(root == null) return;
+        
+        inorder(root.left , list);
+        list.add(root.val);
+        inorder(root.right , list);
+    }
+    
+    public void merge(List<Integer> t1 , List<Integer> t2 , List<Integer> res){
+        int i = 0 , j = 0;
+        
+        while(i < t1.size() && j < t2.size()){
+            if(t1.get(i) <= t2.get(j)){
+                res.add(t1.get(i));
+                i++;
+            }
+            else{
+                res.add(t2.get(j));
+                j++;
+            }
+        }
+        
+        while(i < t1.size()){
+            res.add(t1.get(i));
+            i++;
+        } 
+        while(j < t2.size()){
+            res.add(t2.get(j));
+            j++;
+        } 
+    }
+}
