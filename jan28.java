fb1351fe63cea01254aa7b1304f1c2515b92d5bd
AnishGupta25 <anishgupta2522@gmail.com>
2022-01-28 18:23:49 +0530

    done

--- jan28.java
+++ jan28.java
@@ -50,4 +50,24 @@ public class jan28 {
             return root.find(word,0);
         }
     }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        
+        int lo = 0 , hi = n - 1;
+        
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[lo] <= nums[mid]){
+                if(nums[lo] <= target && nums[mid] >= target) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            else if(nums[mid] <= nums[hi]){
+                if(nums[mid] <= target && nums[hi] >= target) lo = mid + 1;
+                else hi = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
