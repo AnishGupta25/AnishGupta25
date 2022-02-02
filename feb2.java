194bb9abcfdb0778eaeeeb677514159a5209bd4a
AnishGupta25 <anishgupta2522@gmail.com>
2022-02-02 23:50:03 +0530

    done

--- feb2.java
+++ feb2.java
@@ -46,5 +46,39 @@ public class feb2 {
             return true;
         }
     }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<Integer>();
+        int n = s.length();
+        int m = p.length();
+        if (n < m)
+            return list;
+        int[] pAlphas = new int[26];
+        for (char c : p.toCharArray()) {
+            pAlphas[c - 'a']++;
+        }
+        int[] sAlphas = new int[26];
+        for (int i = 0; i < m; i++) {
+            sAlphas[s.charAt(i) - 'a']++;
+        }
+        if (compareAlphaList(pAlphas, sAlphas))
+            list.add(0);
+        for (int i = m; i < n; i++) {
+            sAlphas[s.charAt(i - m) - 'a']--;
+
+            char c = s.charAt(i);
+            sAlphas[c - 'a']++;
+            if (compareAlphaList(sAlphas, pAlphas))
+                list.add(i - m + 1);
+        }
+        return list;
+    }
+
+    private boolean compareAlphaList(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++)
+            if (a[i] != b[i])
+                return false;
+        return true;
+    }
     
 }
