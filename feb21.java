cbd60c36cffa8b51cc39b304a786483dea9273c2
AnishGupta25 <anishgupta2522@gmail.com>
2022-02-21 10:22:58 +0530

    done

---
+++ feb21.java
@@ -0,0 +1,16 @@
+public class feb21 {
+    public int majorityElement(int[] nums) {
+        int idx = 0 , freq = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[idx]) freq++;
+            else freq--;
+            
+            if(freq < 0){
+                idx = i;
+                freq = 1;
+            }
+        }
+        return nums[idx];
+    }
+}
